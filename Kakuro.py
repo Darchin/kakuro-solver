81cd713b53877d8c40069e4b78d45ca4c9b830f8
Mehdi Rejali <51312282+Darchin@users.noreply.github.com>
2023-12-08 15:06:17 +0330

    pre-overhaul code

--- Kakuro.py
+++ Kakuro.py
@@ -1,9 +1,13 @@
 from partitioner import Partitioner
 from operator import attrgetter
+import sys
+sys.setrecursionlimit(1000000)
+INPUT_BOARD_MATRIX = []
 class Utils:
     # DEFAULT DISPLAY SETTINGS
     COLORS = {'RED':'\033[91m','EMERALD':'\033[92m','YELLOW':'\033[93m','BLUE1':'\033[94m','PINK':'\033[95m','BLUE2':'\033[96m','WHITE':'\033[97m'}
     BLOCK_STYLE = '\u25A0'
+    COLOR_LAST_ASSIGNMENT = COLORS['RED']
     COLOR_H = COLORS['YELLOW']
     COLOR_V = COLORS['BLUE2']
     COLOR_SEP = COLORS['WHITE']
@@ -20,6 +24,12 @@ class Utils:
     def printBoard(board_display_matrix):
         for i in board_display_matrix:
             print('\t'.join(map(str, i)))
+    def findPartitionOffset(perp_group, shared_tile):
+        if perp_group.orientation == 'H':
+            offset = shared_tile.col - perp_group.tiles[0].col
+        else:
+            offset = shared_tile.row - perp_group.tiles[0].row
+        return offset
 class Kakuro:
     class Tile:
         def __init__(self, row, col):
@@ -38,15 +48,20 @@ class Kakuro:
             self.partitions = []
             self.partition_mask = []
             self.ratio = 0
-        def calculateRatio(self):
-            self.ratio = min(9-(self.rule/len(self.tiles)),(self.rule/len(self.tiles))-1)
+        def calculateRatio(self, assigned = False):
+            if assigned:
+                self.ratio = 999
+            else: 
+                self.ratio = min(9-(self.rule/len(self.tiles)),(self.rule/len(self.tiles))-1)
         def generatePartitions(self):
             self.partitions = Partitioner.getOrderedPartitions(self.rule, len(self.tiles))
             self.partition_mask = Partitioner.generatePartitionMask(self.partitions)
-        def generateRestrictedPartitions(self, partition_index, shared_digit):
-            incomplete_partitions = Partitioner.getOrderedPartitions( (self.rule-shared_digit), len(self.tiles)-1)
-            for p in incomplete_partitions:
-                p.insert(partition_index, shared_digit)
+        def generateRestrictedPartitions(self, partition_offset, shared_digit):
+            partitions = Partitioner.getOrderedPartitions( (self.rule-shared_digit), len(self.tiles)-1)
+            for p in partitions:
+                p.insert(partition_offset, shared_digit)
+            self.partitions = partitions
+            self.partition_mask = Partitioner.generatePartitionMask(partitions)
         def __str__(self):
             string = f'Group ID: {self.id}-{self.orientation}\nRule = {self.rule}\nTiles = {self.tiles}\n'
             if self.ratio != 0: string += f'Ratio = {self.ratio}\n'
@@ -96,16 +111,105 @@ class Kakuro:
             groups = [h_groups]
             groups.append(v_groups)
         return Kakuro(groups, tile_map), board_display_matrix
-    def calculateRatios(self):
+    def calculateRatios(self, assignments = {}):
         for g_list in self.groups:
             for g in g_list:
-                g.calculateRatio()
-    def selectGroup(self):
+                if f'{g.orientation}{g.id}' in assignments:
+                    g.calculateRatio(True)
+                else: g.calculateRatio()
+    def selectGroup(self) -> Group:
         min_h_group = min(self.groups[0], key=attrgetter('ratio'))
         min_v_group = min(self.groups[1], key=attrgetter('ratio'))
         return min(min_h_group,min_v_group, key=attrgetter('ratio'))
-    def solve(self, new_csp, assignments = []):
-        pass
+    def findGroup(self, group_pair) -> Group:
+        group_id = int(group_pair[1:])
+        if group_pair[0] == 'H':
+            index = 0
+        else:
+            index = 1
+        return self.groups[index][group_id]
+    def updateBoard(self, assignments, group_key):
+        updated_board = INPUT_BOARD_MATRIX.copy()
+        for var, val in assignments.items(): # 'var' is a group {orientation}{id} pair - like H2
+            i = 0
+            assigned_group = self.findGroup(var)
+            for num in val: # 'val' is a partition
+                row = assigned_group.tiles[i].row
+                col = assigned_group.tiles[i].col
+                updated_board[row][col] = str(num)
+                i += 1
+
+        last_assigned_group = self.findGroup(group_key)
+        i = 0
+        for num in assignments[group_key]:
+            row = last_assigned_group.tiles[i].row
+            col = last_assigned_group.tiles[i].col
+            updated_board[row][col] = Utils.COLOR_LAST_ASSIGNMENT + str(num) + Utils.END_COLOR
+            i += 1
+        return updated_board
+    def getCorrespondingGroup(self, other_group):
+        if other_group.orientation == 'H':
+            index = 0
+        else:
+            index = 1
+        return self.groups[index][other_group.id]
+    def solve(self, assignments = {}):
+        print(assignments)
+        L = len(assignments)
+        if L == len(self.groups[0]) + len(self.groups[1]):
+            return assignments
+        # Create a new, separate copy of the given game board
+        # dummy_board = Kakuro(self.groups, self.tile_map)
+
+        # Choose a variable (group in this case) according to ratios
+        self.calculateRatios(assignments)
+        group = self.selectGroup()
+        group_key = f'{group.orientation}{group.id}'
+        # Generate chosen group's partitions
+        group.generatePartitions()
+
+        # Find colliding groups
+        perp_groups = []
+        offsets = []
+        for tile in group.tiles:
+            # Perp is short for 'perpendicular' - Basically we find vertical groups sharing a tile with a horizontal group and vice-versa
+            perp_group = self.getPerpendicularGroup(group, tile)
+            perp_groups.append(perp_group)
+            partition_offset = Utils.findPartitionOffset(perp_group, tile)
+            offsets.append(partition_offset)
+
+        # Iterate over each partition, adding it to assignments and generating the restricted partition list for the affected groups;
+        # Then check whether if a group's partition list becomes empty. If so, remove assignment.
+        for p in group.partitions:
+            next_board = Kakuro(self.groups, self.tile_map)
+            next_board.getCorrespondingGroup(group).partitions = [p]
+            inconsistent_partition = False
+            assignments[group_key] = p.copy()
+            # Utils.printBoard(next_board.updateBoard(assignments, group_key))
+
+            for i in range(len(perp_groups)):
+                shared_digit = p[i]
+                perp_groups[i].generateRestrictedPartitions(partition_offset, shared_digit)
+                if len(perp_groups[i].partitions) == 0:
+                    inconsistent_partition = True
+                    break
+            if inconsistent_partition:
+                del assignments[group_key]
+                continue
+            # If this point is reached, the current partition has been consistent, therefore we call solve again and move onto deeper assignment
+            if len(assignments) != L:
+                return result
+            result = next_board.solve(assignments)
+            return result
+
+    def getPerpendicularGroup(self, original_group, shared_tile):
+        group_pair = self.tile_map[shared_tile.row, shared_tile.col]
+        if original_group.orientation == 'H':
+            index = 1
+        else: 
+            index = 0
+        return self.groups[index][group_pair[index]]
+    
     def printGroups(self):
         h_groups = self.groups[0]
         v_groups = self.groups[1]
@@ -121,10 +225,12 @@ def main():
     easy_kakuro_1 = "1 2 4 19,1 6 7 10,2 1 7 39,3 1 2 15,3 4 5 10,4 2 3 16,4 5 6 4,5 3 4 9,5 6 7 12,6 1 7 35,7 1 2 16,7 4 6 7;"+\
     "1 2 3 16,1 6 7 14,2 1 4 30,2 6 7 16,3 1 2 4,3 4 6 23,4 1 3 24,4 5 7 6,5 2 4 9,5 6 7 4,6 1 2 4,6 4 7 10,7 1 2 16,7 5 6 16"
     game_board, board_display_matrix = Kakuro.createBoardFromString(N, easy_kakuro_1)
-    game_board.calculateRatios()
-    print(game_board.selectGroup())
+    global INPUT_BOARD_MATRIX
+    INPUT_BOARD_MATRIX = board_display_matrix
+    # print(game_board.selectGroup())
     # Utils.printBoard(board_display_matrix)
     # game_board.printGroups()
+    game_board.solve()
 
 if __name__ == '__main__':
     main()
