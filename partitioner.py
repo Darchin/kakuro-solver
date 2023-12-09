a3ec5908554ebaf1027849f5a0eec159b053817f
Mehdi Rejali <51312282+Darchin@users.noreply.github.com>
2023-12-09 22:17:30 +0330

    bug fixes

--- partitioner.py
+++ partitioner.py
@@ -3,6 +3,7 @@ class Partitioner:
         g_list = []
 
         if k == 1: 
+            if N < 1: return g_list
             return [g_list + [*list, N]]
         
         for i in range(1,10): # 'i' is from 1 to 9
@@ -22,7 +23,7 @@ class Partitioner:
             else:
                 i += 1
         return partitions
-    def getOrderedPartitions(N, k):
+    def getOrderedPartitions(N, k) -> list:
         return Partitioner.remove_duplicates(Partitioner.partition(N, k))
     def flatten(list):
         return [item for sublist in list for item in sublist]
@@ -33,4 +34,4 @@ class Partitioner:
             if i in flattened_partition_list:
                 partition_mask[i-1] = True
         return partition_mask
-print(Partitioner.getOrderedPartitions(30,4))
+# print(Partitioner.getOrderedPartitions(4,0))
